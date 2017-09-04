2ef07ea4be184cc6c986e004009e4f3a3e3c13a5
NosillaMarie <aliederman@icloud.com>
2017-09-04 11:06:15 -0400

    trying to get the survey form to submit.  Made changes rying again

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,15 @@
+//Loading data from the specifies location
 var friends = require("../data/friends");
 
 module.exports = function (app) {
+
+    //API GET request handles when a user visits a link.
+    //this like will return a JSON object of the information collected through the survey.
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
+
+    //this API POST request handles when a user submits data to the server through the html form.
 
     app.post("/api/friends", function (req, res) {
         friends.push(req.body);
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,11 @@
+//Dependencies
+//Path is used to get the correct file path for the html file 
 var path = require("path");
 
+
 module.exports = function (app) {
-    //routes to survey page
+    
+    //routes to survey page using the HTML GET request
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
