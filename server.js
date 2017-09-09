7220491313a3c682e4a48cefa8fe025ee9a7b7b4
NosillaMarie <aliederman@icloud.com>
2017-09-08 21:03:50 -0400

    still can't figure out what this thing doesn't like

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
 
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, './app/public')));
-
-app.use(express.static(path.join(__dirname, 'server.js')));
+app.use(express.static(__dirname, './app/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
