0db9930599197e79a17832c376ba4dbea3355dc5
NosillaMarie <aliederman@icloud.com>
2017-09-09 09:30:42 -0400

    fixing what I broke

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,29 +14,29 @@ module.exports = function (app) {
         app.post("/api/friends", function (req, res) {
             var user_info = req.body;
 
-            var bestMatch = {
-                name: "",
-                photo: "",
-                friendDifference: 1000
-            };
-            var userScores = user_info.scores;
-
-            console.log(userScores);
-
-            var totalDifference = 0;
-            for (var i = 0; i < friends.length; i++) {
-                console.log(friends[i]);
-                totalDifference = 0;
-            }
-            for (var j = 0; i < friends[i].scores[j]; j++) {
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-                if (totalDifference <= bestMatch.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-                }
-            }
+//            var bestMatch = {
+//                name: "",
+//                photo: "",
+//                friendDifference: 1000
+//            };
+//            var userScores = user_info.scores;
+//
+//            console.log(userScores);
+//
+//            var totalDifference = 0;
+//            for (var i = 0; i < friends.length; i++) {
+//                console.log(friends[i]);
+//                totalDifference = 0;
+//            }
+//            for (var j = 0; i < friends[i].scores[j]; j++) {
+//                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+//
+//                if (totalDifference <= bestMatch.friendDifference) {
+//                    bestMatch.name = friends[i].name;
+//                    bestMatch.photo = friends[i].photo;
+//                    bestMatch.friendDifference = totalDifference;
+//                }
+//            }
 
 
 
